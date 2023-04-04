2dfcfcf129ba969bd586c155cb2e0367b9bb1ef0
Gabriel Badaró <grbadas@gmail.com>
2023-04-03 21:43:19 -0300

    Controller

--- com/badas/springboot/controller/AlunoController.java
+++ com/badas/springboot/controller/AlunoController.java
@@ -1,5 +1,32 @@
 package com.badas.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.badas.springboot.model.Aluno;
+import com.badas.springboot.repository.AlunoRepository;
+
+@RestController
+@RequestMapping("/api/v1/alunos")
 public class AlunoController {
+	
+	@Autowired
+	private AlunoRepository alunoRepository;
+	
+	@GetMapping
+	public List<Aluno> getAllAlunos() {
+		return alunoRepository.findAll();
+	}
+	
+	@PostMapping
+	public Aluno createAluno(@RequestBody Aluno aluno) {
+		return alunoRepository.save(aluno);
+	}
 
 }
