3c5f47a8801246240b5a70e43b4c390850270c1a
Gabriel Badaró <grbadas@gmail.com>
2023-04-05 00:50:06 -0300

    Controller

--- com/badas/springboot/controller/AlunoController.java
+++ com/badas/springboot/controller/AlunoController.java
@@ -3,12 +3,15 @@ package com.badas.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.badas.springboot.exception.ResourceNotFoundException;
 import com.badas.springboot.model.Aluno;
 import com.badas.springboot.repository.AlunoRepository;
 
@@ -27,6 +30,24 @@ public class AlunoController {
 	@PostMapping
 	public Aluno createAluno(@RequestBody Aluno aluno) {
 		return alunoRepository.save(aluno);
+	}
+	
+	public ResponseEntity<Aluno> getAlunoByIdEntity(@PathVariable Long id) {
+		Aluno aluno = alunoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Aluno não encontrado" + id));
+		return ResponseEntity.ok(aluno);
+	}
+	
+	public Aluno updateAluno(@PathVariable Long id, @RequestBody Aluno alunoDetails) {
+		Aluno aluno = alunoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Aluno não encontrado" + id));
+		
+		aluno.setTurmas(alunoDetails.getTurmas());
+		
+		return alunoRepository.save(aluno);
+	}
+	
+	public void deleteAluno(@PathVariable Long id) {
+		Aluno aluno = alunoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Aluno não encontrado" + id));
+		alunoRepository.delete(aluno);
 	}
 
 }
